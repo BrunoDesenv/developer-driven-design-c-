9c7ef8e35ae8ea26a2597dd6c78d133fa6d636b6
Bruno Santos <bru_blink@hotmail.com>
2017-07-07 01:46:43 -0300

    Add method teste

---
+++ Exemplo.Test.MVC/ProductTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Exemplo.Test.MVC
+{
+    [TestClass]
+    public class ProductTest
+    {
+        [TestMethod]
+        public void Register()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost/Product/Register");
+            driver.Manage().Window.Maximize();
+
+            IWebElement name = driver.FindElement(By.Id("name"));
+            IWebElement price = driver.FindElement(By.Id("price"));
+
+            name.SendKeys("Product 1");
+            price.SendKeys("2.50");
+
+            var enter = driver.FindElement(By.Id("enter"));
+            enter.Equals("Enter");
+
+            enter.Click();
+
+            driver.Quit();
+        }
+    }
+}
